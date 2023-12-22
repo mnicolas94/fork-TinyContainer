734219bb12d3f69a30aec8ec1dd902c430ecf42e
mnicolas94 <miguelcok27@gmail.com>
2023-12-22 11:02:54 -0500

    feat: implement TinyContainerInstaller with container level selection

---
+++ Assets/TinyContainer/Runtime/TinyContainerInstaller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jnk.TinyContainer
+{
+    public enum ContainerLevel
+    {
+        Global,
+        Scene,
+        Local
+    }
+    
+    [AddComponentMenu("TinyContainer/TinyContainer Object Installer")]
+    public class TinyContainerInstaller : MonoBehaviour
+    {
+        [SerializeField] private ContainerLevel _level;
+        
+        [SerializeField]
+        private List<Object> objects;
+
+        protected void Awake()
+        {
+            var container = GetContainer();
+
+            foreach (var obj in objects)
+                container.Register(obj.GetType(), obj);
+        }
+
+        private TinyContainer GetContainer()
+        {
+            switch (_level)
+            {
+                case ContainerLevel.Global:
+                    return TinyContainer.Global;
+                case ContainerLevel.Scene:
+                    return TinyContainer.ForSceneOf(this);
+                case ContainerLevel.Local:
+                default:
+                    return TinyContainer.For(this);                            
+            }
+        }
+    }
+}
